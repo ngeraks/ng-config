7e1ef0752d87fca66f92da5bcbc85d4fcaeb06e4
ni <ni@pop-os.localdomain>
2022-04-15 13:34:19 +0300

    ni@pop-os Fri 15 Apr 13:34:19 MSK 2022

--- sync.sh
+++ sync.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 push(){
+  echo push
   git add .
-  git commit -m $(whoami)@$(localhost) $(date)
+  git commit -m "$(whoami)@$(hostname) $(date)"
   git push
 }
 pull(){
-
+  echo pull
+  git pull
 }
 
-
+local_change=$(git status --porcelain --untracked-files=no | wc -l)
+if [ $local_change -gt 0 ]; then push; fi
+remote_change=$(git fetch --dry-run | wc -l)
+if [ $remote_change -gt 0 ]; then pull; fi
