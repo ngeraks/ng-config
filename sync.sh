1e78bdc69538f818eafed16f55bf716993ad768c
ni <ni@pop-os.localdomain>
2022-04-19 21:57:56 +0300

    ni@len Tue 19 Apr 21:57:56 MSK 2022

--- sync.sh
+++ sync.sh
@@ -13,7 +13,9 @@ push(){
   git push
 }
 
-local_change=$(git status --porcelain --untracked-files=no | wc -l)
-if [ $local_change -gt 0 ]; then push; fi
-remote_change=$(git fetch --dry-run | wc -l)
-if [ $remote_change -gt 0 ]; then pull; fi
+#local_change=$(git status --porcelain --untracked-files=no | wc -l)
+#if [ $local_change -gt 0 ]; then 
+  push #; fi
+#remote_change=$(git fetch --dry-run | wc -l)
+#if [ $remote_change -gt 0 ]; then 
+  pull #; fi
